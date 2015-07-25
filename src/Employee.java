3936ab48071183a48be44e33be61d6f2a512a3dc
t3hstiv <stevenbarrett89@gmail.com>
2015-07-25 17:21:31 -0500

    DB info
    Added SQL commands to create database.

--- src/Employee.java
+++ src/Employee.java
@@ -1,3 +1,37 @@
+/*
+CREATE TABLE `employee` (
+  `E_id` int(11) NOT NULL,
+  `E_fname` varchar(45) NOT NULL,
+  `E_lname` varchar(45) NOT NULL,
+  `E_birthdate` date DEFAULT NULL,
+  `E_address` varchar(75) DEFAULT NULL,
+  `E_phone` varchar(45) DEFAULT NULL,
+  `E_marital_status` varchar(7) NOT NULL,
+  `E_withholding` int(11) NOT NULL,
+  `E_wage_rate` double NOT NULL,
+  `E_SSN` varchar(11) DEFAULT NULL,
+  PRIMARY KEY (`E_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+CREATE TABLE `payroll` (
+  `P_id` int(11) NOT NULL,
+  `E_id` int(11) NOT NULL,
+  `P_gross` double NOT NULL,
+  `P_FIT` double NOT NULL,
+  `P_SIT` double NOT NULL,
+  `P_social_security` double NOT NULL,
+  `P_medicare` double NOT NULL,
+  `P_net` double NOT NULL,
+  `P_start_date` date NOT NULL,
+  `P_end_date` date NOT NULL,
+  PRIMARY KEY (`P_id`),
+  KEY `payroll_to_employee_idx` (`E_id`),
+  CONSTRAINT `payroll_to_employee` FOREIGN KEY (`E_id`) REFERENCES `employee` (`E_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+*/
+
+
 import java.sql.*;
 
 public class Employee 
