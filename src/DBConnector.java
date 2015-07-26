87c6822ae6462804a63f969106ecc5420f4b93fd
t3hstiv <stevenbarrett89@gmail.com>
2015-07-25 22:56:34 -0500

    Calculator/Renaming
    Put up the calculator class. Doesn't actually do anything, but has
    methods with applicable calculations. Renamed Employee.java to
    DBConnector.java, and made appropriate changes to TestDriver. Insert in
    DBConnector works

--- src/DBConnector.java
+++ src/DBConnector.java
@@ -30,11 +30,10 @@ CREATE TABLE `payroll` (
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 */
-
-
 import java.sql.*;
 
-public class Employee 
+
+public class DBConnector 
 {
 	static Connection conn;
 	
@@ -52,7 +51,7 @@ public class Employee
 				rownum++;
 			}
 			System.out.println(rownum);
-			String sqlStatement ="INSERT INTO ippdb.employee " + "VALUES (" + rownum + ", 'Antonio', 'Monreal', '1990-05-07', '123 blah st', '(773)123-4567', 'single', 2, 100.0, '123-54-6798')";
+			String sqlStatement ="INSERT INTO ippdb.employee " + "VALUES (" + rownum + ", '" + fname + "', '" + lname + "', '" + bdate + "', '" + address + "', '" + phone + "', '" + marital + "', " + withholding + ", " + wage + ", '" + ssn + "')";
 			stmt.executeUpdate(sqlStatement);
 			stmt.close();
 			conn.close();
@@ -71,7 +70,6 @@ public class Employee
 			conn = DBConnect.getConnection();
 			Statement stmt = conn.createStatement();
 			rs = stmt.executeQuery("SELECT E_id, E_fname, E_lname FROM ippdb.employee ORDER BY E_id");
-			conn.close();
 			return rs;
 		}
 		catch (Exception e)
--- src/TestDriver.java
+++ src/TestDriver.java
@@ -5,14 +5,14 @@ public class TestDriver
 
 	public static void main(String[] args) 
 	{
-		System.out.println(Employee.getWage(2));
-		Employee.setWage(2, 25.0);
-		System.out.println(Employee.getWage(2));
-		Employee.addEmployee("Antonio", "Monreal", "1952-01-18", "123 Fake St.", "(773)123-4567", "single", 2, 100.0, "123-45-6789");
-		System.out.println(Employee.getWage(4));
+		System.out.println(DBConnector.getWage(2));
+		DBConnector.setWage(2, 25.0);
+		System.out.println(DBConnector.getWage(2));
+		DBConnector.addEmployee("Antonio", "Monreal", "1952-01-18", "123 Fake St.", "(773)123-4567", "single", 2, 100.0, "123-45-6789");
+		System.out.println(DBConnector.getWage(4));
 		try {
 			
-		ResultSet rs = Employee.listEmployee();
+		ResultSet rs = DBConnector.listEmployee();
 		while(rs.next())
 		{
 			int stuff = rs.getInt("E_id");
